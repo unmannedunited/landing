8b8d880884c78311ba377a82b4b646779d00686c
Agus <m.agustina.nahas@gmail.com>
2025-09-10 18:45:25 -0300

    fix: update asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "",
-    assetPrefix: "",
+    basePath: "/unmanned",
+    assetPrefix: "/unmanned",
 };
 
 export default nextConfig;
