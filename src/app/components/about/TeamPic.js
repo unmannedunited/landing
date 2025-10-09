e7926ee607009fe81008fb70adda1ae0ada69c38
Agus <m.agustina.nahas@gmail.com>
2025-10-08 21:47:34 -0300

    about us a medias img fix

--- src/app/components/about/TeamPic.js
+++ src/app/components/about/TeamPic.js
@@ -14,9 +14,9 @@ function TeamPic() {
   return (
     <div className="relative" >
      <img src={getImageUrl("/about/about-team.png")} alt="Team Pic" className="w-full h-full" />
-     <img src={getImageUrl("/about/about-stamp.png")} className="absolute left-0 top-[-4px]" style={{ transform: `translate3d(${parallax.x}px, 0px, 0)`, willChange: 'transform'}} />
-     <img src={getImageUrl("/about/about-stamp.png")} className="absolute left-[35%] top-[-4px]" style={{ transform: `translate3d(${parallax.x}px, 0px, 0)`, willChange: 'transform'}} />
-     <img src={getImageUrl("/about/about-stamp.png")} className="absolute left-[70%] top-[-4px]" style={{ transform: `translate3d(${parallax.x}px, 0px, 0)`, willChange: 'transform'}} />
+     <img src={getImageUrl("/about/about-stamp.png")} alt="" className="absolute left-0 top-[-4px]" style={{ transform: `translate3d(${parallax.x}px, 0px, 0)`, willChange: 'transform'}} />
+     <img src={getImageUrl("/about/about-stamp.png")} alt="" className="absolute left-[35%] top-[-4px]" style={{ transform: `translate3d(${parallax.x}px, 0px, 0)`, willChange: 'transform'}} />
+     <img src={getImageUrl("/about/about-stamp.png")} alt="" className="absolute left-[70%] top-[-4px]" style={{ transform: `translate3d(${parallax.x}px, 0px, 0)`, willChange: 'transform'}} />
      <img src={getImageUrl("/about/about-logo.png")} alt="Team Pic" 
       className="absolute left-[70%] top-[40%]" 
       style={{ transform: `rotate(-25deg)`, willChange: 'transform'}} />
